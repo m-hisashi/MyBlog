167c204ae90434bdf8edba28239459993ce11fe1
vagrant <vagrant@localhost.localdomain>
2019-01-27 05:12:38 +0000

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_130543) do
+ActiveRecord::Schema.define(version: 2019_01_27_050901) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
